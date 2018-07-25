8ddf821ca469320767d458778aa1b96927df4959
honmono-sakurai <41695327+honmono-sakurai@users.noreply.github.com>
2018-07-26 00:28:34 +0900

    Create check.py

---
+++ check.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import sys
+sys.path.append('/path/to/dir')
+
+import pyocr
+import pyocr.builders
+
+tls = pyocr.get_available_tls()
+if len(tls) == 0:
+    sys.exit(1)
+    
+tool = tls[0]
+langage = tool.get_available_languages()
+extracted_txt = tool.image_to_string(
+    Image.open('target.png'),
+    lang='jpn',
+    builder=pyocr.builders.TextBuilder()
+)
+
+if 'sakurai'.count('fuga') == 0:
+    print('you are suck')
+else:
+    print('awesome!')
